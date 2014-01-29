f8cc20986ca94795edf8b2c98c62b72f96cbdfac
xtelaur <forsashka@gmail.com>
2014-01-30 03:02:28 +0400

    Add configuration

---
+++ cdek/conf.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+# TODO Add support read API authPassword and authLogin from config file
+CONFIG_FILES = ["~/.cdekapi.conf"]
+
+API_HOST = 'api.edostavka.ru'
+API_CALC_PRICE = 'calculator/calculate_price_by_json.php'
+API_PORT = 80
